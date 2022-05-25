9bd1b982d6390feefb82e9f76c6afd1769999b5e
aleze89 <68794504+aleze89@users.noreply.github.com>
2022-05-25 14:27:32 -0300

    promedio

--- Clase7/Program.cs
+++ Clase7/Program.cs
@@ -1,8 +1,8 @@
-﻿//1) Pida 10 números al usuario.  *INGRESO DE DATOS (FOR ?)
-//2) Obtener la suma de todos los números ingresados.  *PROCESO
-//3) Obtener cual es el mayor de todos los números.  *PROCESO (IF ?)
-//4) Obtener cual es el menor de todos los números.  *PROCESO (IF ?)
-//5) Obtener el promedio de todos los números.  *PROCESO
+﻿//1) Pida 10 números al usuario.  *INGRESO DE DATOS (FOR ?)  +++
+//2) Obtener la suma de todos los números ingresados.  *PROCESO +++ 
+//3) Obtener cual es el mayor de todos los números.  *PROCESO (IF ?)  +++ 
+//4) Obtener cual es el menor de todos los números.  *PROCESO (IF ?) +++
+//5) Obtener el promedio de todos los números.  *PROCESO +++
 //6) Mostrar todos los números ingresados por pantalla.  *SALIDA DE DATOS
 //7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.  *SALIDA DE DATOS
 
@@ -40,7 +40,7 @@ for (int i = 0; i < numerosIngresados.Length; i++)
 
 }
 
-Console.WriteLine($"el maximo es: {mayor}");
+Console.WriteLine($"El maximo es: {mayor}");
 
 //menor
 
@@ -58,5 +58,10 @@ for (int i = 0; i < numerosIngresados.Length; i++)
 
 }
 
-Console.WriteLine($"el minimo es: {menor}");
+Console.WriteLine($"El minimo es: {menor}");
+
+//promedio 
+
+var promedio = suma / numerosIngresados.Length;
+Console.WriteLine($"El promedio de los numeros ingresados es: {promedio}");
 
