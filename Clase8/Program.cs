7f5497b4e904cc7b3a3574a465fae1f6ffdbe2a2
aleze89 <68794504+aleze89@users.noreply.github.com>
2022-05-25 22:36:43 -0300

    muestra de datos

--- Clase8/Program.cs
+++ Clase8/Program.cs
@@ -1,8 +1,8 @@
 ﻿//1) Crear una matriz de dos dimensiones de tipo int llamada numeros,  +++
 //2) Determinar el tamaño de cada dimansion (fila, columnas) mediante valores ingresados por pantalla *INGRESO DE DATOS  +++
 //3) Declarar un vector de tipo double llamado promedios  +++
-//4) Recorrer la matriz para su carga con elementos de tipo int *BUCLE (FOR ?)
-//5) Recorrer la matriz para mostrar cada valor de la matriz *BUCLE (FOR ?)
+//4) Recorrer la matriz para su carga con elementos de tipo int *BUCLE (FOR ?)  +++
+//5) Recorrer la matriz para mostrar cada valor de la matriz *BUCLE (FOR ?)   +++
 //6) Calcular el promedio de cada columna y asignarlo a la posicion correspondiente dentro del vector promedios *PROCESO
 //7) Mostrar los promedios recorriendo el vector promedios *MUESTRA DE DATOS
 
@@ -25,6 +25,8 @@ int lengthColumnas = numeros.GetUpperBound(1) + 1;
 
 double[] promedios = new double[lengthFilas];  // PENDIENTE
 
+//  Ingreso de datos a la matriz
+
 for (int columna= 0; columna < lengthColumnas; columna++)
 {
     Console.WriteLine();
@@ -35,6 +37,21 @@ for (int columna= 0; columna < lengthColumnas; columna++)
     {
         Console.WriteLine($"Ingresa los valores correspondientes a la fila n° : {fila + 1}");
         numeros[fila, columna] = int.Parse(Console.ReadLine());
+    }
+}
+
+//  Muestra de datos de la matriz
+
+for (int columna = 0; columna < lengthColumnas; columna++)
+{
+    Console.WriteLine("-----Muestra de datos-----");
+    Console.WriteLine($"-----Te encuentras en la columna n°: {columna + 1 }-----");
+    Console.WriteLine();
+
+    for (int fila = 0; fila < lengthFilas; fila++)
+    {
+        Console.WriteLine($"Te encuentras en la fila n° : {fila + 1}");
+        Console.WriteLine($"Tiene almacenado: {numeros[fila, columna]}");
     }
 }
 
