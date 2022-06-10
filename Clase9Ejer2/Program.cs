144ddf60f41aa9f1cf08189a1775d5cd11abca19
aleze89 <68794504+aleze89@users.noreply.github.com>
2022-06-10 16:30:32 -0300

    sigo con problemas al crear los valores acordes a sus columnas

--- Clase9Ejer2/Program.cs
+++ Clase9Ejer2/Program.cs
@@ -1,6 +1,6 @@
 ﻿//      Generar un programa que cree un cartón de bingo aleatorio y lo muestre por pantalla.
 
-//1)    Cartón de 3 filas por 9 columnas.
+//1)    Cartón de 3 filas por 9 columnas.  +++
 //2)    El cartón debe tener 15 números y 12 espacios en blanco.
 //3)    Cada fila debe tener 5 números.
 //4)    Cada columna debe tener 1 o 2 números.
@@ -8,24 +8,43 @@
 //6)    La primer columna contiene los números del 1 al 9,
 //      la segunda del 10 al 19, la tercera del 20 al 29,
 //      así sucesivamente hasta la última columna la cual contiene del 80 al 90.
-//7)    Mostrar el carton por pantalla.
+//7)    Mostrar el carton por pantalla. +++
 
 // Variables
 
 var fila = 3;
 var columna = 9;
+var contador = 1;
 
 var cartonBingo = new int[fila,columna];
 Random aleatorio = new Random();
 
-
+//  Creacion valores de carton
 
 for (int f = 0; f < fila; f++)
 {
     for (int c = 0; c < columna; c++)
     {
-        cartonBingo[f,c] = aleatorio.Next(90);
-        Console.Write($"{cartonBingo[f,c]:00} ");
+        while (contador != columna)
+        {
+        cartonBingo[f,c] = aleatorio.Next(contador*9);
+
+        contador++;
+        break;
+        }
+        
+    }
+    
+
+}
+
+//  Mostrar valores de carton
+
+for (int f = 0; f < fila; f++)
+{
+    for (int c = 0; c < columna; c++)
+    {
+        Console.Write($"{cartonBingo[f, c]:00} ");
     }
     Console.WriteLine();
 
