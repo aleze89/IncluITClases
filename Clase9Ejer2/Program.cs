dbf0581c0bdad96b0ac2e476b4efc23ebaa4dd09
aleze89 <68794504+aleze89@users.noreply.github.com>
2022-07-13 12:21:02 -0300

    numeros ordenados de menor a mayor en cada columna...utilizando ordenamiento por burbuja

--- Clase9Ejer2/Program.cs
+++ Clase9Ejer2/Program.cs
@@ -10,88 +10,105 @@
 //      así sucesivamente hasta la última columna la cual contiene del 80 al 90.
 //7)    Mostrar el carton por pantalla. +++
 
-// Variables
+    // Variables
 
-var fila = 3;
-var columna = 9;
-var contador = 1;
+    var fila = 3;
+    var columna = 9;
+    var contador = 1;
 
-var cartonBingo = new int[fila,columna];
-var aleatorio = new Random();
+    var cartonBingo = new int[fila,columna];
+    var aleatorio = new Random();
 
-//  Creacion valores de carton discriminando columnas
-
-for (int c = 0; c < columna; c++)
-{
-    for (int f = 0; f < fila; f++)
-    {
-        int numeroCarton = 0;
-        bool numeroRepetido = false;
-
-        while (!numeroRepetido)
-        {
-            if (c==0) // Columna 1
-            {
-                numeroCarton = aleatorio.Next(1, 10); // 1 al 9
-            }
-            else if (c==8) //Columna 9
-            {
-                numeroCarton = aleatorio.Next(80, 91); // 80 al 90
-            }
-            else // Todas las demas Columnas
-            {
-                numeroCarton = aleatorio.Next(c * 10, c * 10 + 10);
-            }
-
-            // Buscamos si el nuemroCarton existe en la columna
-
-            numeroRepetido = true;
-            for (int f2 = 0; f2 < 3; f2++)
-            {
-                if (cartonBingo[f2,c]==numeroCarton)
-                {
-                    numeroRepetido = false;
-                    break;
-                }
-            }
-            // Si salio del bucle y no encontro repetidos,
-            //numeroRepetido = true y sale del bucle while
-
-        }
-
-        cartonBingo[f, c] = numeroCarton;
-    }
-    
-
-}
-
-//  Mostrar valores de carton
-
-//Console.WriteLine(" ----------------------------------------------- ");
-for (int f = 0; f < fila; f++)
-{
-    Console.WriteLine(" ----------------------------------------------- ");
+    //  Creacion valores de carton discriminando columnas
 
     for (int c = 0; c < columna; c++)
     {
-        if (c==0)
+        for (int f = 0; f < fila; f++)
         {
-            Console.Write(" | ");
-        }
+            int numeroCarton = 0;
+            bool numeroRepetido = false;
 
-        if (cartonBingo[f,c]==0)
-        {
-            Console.Write("| ");
+            while (!numeroRepetido)
+            {
+                if (c==0) // Columna 1
+                {
+                    numeroCarton = aleatorio.Next(1, 10); // 1 al 9
+                }
+                else if (c==8) //Columna 9
+                {
+                    numeroCarton = aleatorio.Next(80, 91); // 80 al 90
+                }
+                else // Todas las demas Columnas
+                {
+                    numeroCarton = aleatorio.Next(c * 10, c * 10 + 10);
+                }
+
+                // Buscamos si el nuemroCarton existe en la columna
+
+                numeroRepetido = true;
+                for (int f2 = 0; f2 < 3; f2++)
+                {
+                    if (cartonBingo[f2,c]==numeroCarton)
+                    {
+                        numeroRepetido = false;
+                        break;
+                    }
+                }
+                // Si salio del bucle y no encontro repetidos,
+                //numeroRepetido = true y sale del bucle while
+
+            }
+
+            cartonBingo[f, c] = numeroCarton;
         }
-        else
+    
+    }
+
+    // Ordeno de menor a mayor los valores de cada columna
+
+    for (int c = 0; c < columna; c++)
+    {
+        for (int f = 0; f < fila; f++)
         {
-            Console.Write($" {cartonBingo[f, c]:00} |");
+            for (int k = f + 1; k < fila; k++)
+            {
+                if (cartonBingo[f,c] > cartonBingo[k,c])
+                {
+                    int aux = cartonBingo[f, c];
+                    cartonBingo[f, c] = cartonBingo[k, c];  // Ordenamiento por burbuja.
+                    cartonBingo[k, c] = aux;
+                }
+            }
         }
     }
-    Console.WriteLine();
 
-}
-Console.WriteLine(" ----------------------------------------------- ");
-Console.WriteLine();
+    //  Mostrar valores de carton
+
+    //Console.WriteLine(" ----------------------------------------------- ");
+    for (int f = 0; f < fila; f++)
+    {
+        Console.WriteLine(" ----------------------------------------------- ");
+
+        for (int c = 0; c < columna; c++)
+        {
+            if (c==0)
+            {
+                Console.Write(" | ");
+            }
+
+            if (cartonBingo[f,c]==0)
+            {
+                Console.Write("| ");
+            }
+            else
+            {
+                Console.Write($" {cartonBingo[f, c]:00} |");
+            }
+        }
+        Console.WriteLine();
+
+    }
+    Console.WriteLine(" ----------------------------------------------- ");
+    Console.WriteLine();
 
 
