647bff81b7e035990165c0b2c60a0f8a46ea38cf
aleze89 <68794504+aleze89@users.noreply.github.com>
2022-07-15 00:04:29 -0300

    comenzando billetera con clases

---
+++ Clase13Billetera/Clase13Billetera/Modelo/Billetera.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase13Billetera.Modelo
+{
+    internal class Billetera
+    {
+        public int BilleteraDe10 { get; set; }
+        public int BilleteraDe20 { get; set; }
+        public int BilleteraDe50 { get; set; }
+        public int BilleteraDe100 { get; set; }
+        public int BilleteraDe200 { get; set; }
+        public int BilleteraDe500 { get; set; }
+        public int BilleteraDe1000 { get; set; }
+        public decimal ImporteTotal { get; set; }
+        public decimal Total()
+        {
+            return ImporteTotal;
+        }
+
+    }
+}
---
+++ Clase13Billetera/Clase13Billetera/Program.cs
@@ -0,0 +1,39 @@
+﻿using Clase13Billetera.Modelo;
+
+namespace Clase13Billetera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+            //BilletesDe10
+            //BilletesDe20
+            //BilletesDe50
+            //BilletesDe100
+            //BilletesDe200
+            //BilletesDe500
+            //BilletesDe1000
+
+            //*Agregar un método que se llame Total del tipo decimal, 
+            //y que devuelva el Importe Total en la billetera teniendo 
+            //en cuenta la cantidad de billetes que se tenga de cada tipo.
+
+            //*Agregar un metodo que se llame Combinar, que reciba como 
+            //parámetro otra billetera y que devuelva una nueva Billetera con la suma o 
+            //combinacion del dinero de ambas billeteras.  
+            //*Una vez combinadas las 2 billeteras(y creada la tercera), 
+            //las 2 primeras billeteras deberan quedar en cero. (Sin billetes)
+
+            Console.WriteLine("Ingrese el billete q posee!");
+            var tipoBillete = Console.ReadLine();
+            
+            Console.WriteLine("Ingrese la cantidad de billetes q posee!");
+            var denominacionBillete = Console.ReadLine();
+
+            var sumarBilletes = new Billetera();
+
+        }
+    }
+}
